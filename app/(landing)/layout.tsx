f5bfadab40d369caaa13ab8717e857163803f4c5
Tushar Patel <tusharsureshpatel666@gmail.com>
2026-02-04 14:43:46 +0530

    hello

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -15,7 +15,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
       <Navbar />
 
       {/* Page Content */}
-      <main className="flex-1 w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-10">
+      <main className="flex-1 justify-center flex items-center w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-10">
         {children}
       </main>
 
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Main from "./components/Main";
+import Hero from "../landing/components/Hero";
+import Camp from "../landing/components/Camp";
 
 const page = () => {
   return (
     <div>
-      <Main />
+      <Hero />
     </div>
   );
 };
